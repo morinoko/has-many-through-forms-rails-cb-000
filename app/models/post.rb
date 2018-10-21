0a486f595b8e27c949faa9a91715aab8a981c30e
Felice Forby <feliceforby@gmail.com>
2018-10-21 22:52:19 +0000

    refactor

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,8 @@ class Post < ActiveRecord::Base
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |category_attribute|
       category = Category.find_or_create_by(category_attribute)
-      self.categories << category
+      self.post_categories.build(category: category)
+      # self.categories << category !Inefficient because it returns all the categories!
     end
   end
 
